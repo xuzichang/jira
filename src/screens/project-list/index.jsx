169ef5adfb141ddea75b5b3de2522a70f8f57ebc
watt <1927236438@qq.com>
2022-09-23 15:19:55 +0800

    feat：添加useMount和useDebounce两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,13 +1,13 @@
 import { List } from "./list"
 import { SearchPanel } from "./search-pannel"
-import React from 'react';
+import React, { useMemo } from 'react';
 import {useEffect,useState} from 'react'
-import { cleanObject } from "utils";
+import { cleanObject, useDebounce, useMount } from "utils";
 import qs from 'qs'
 /*
  * @Description: 
  * @Date: 2022-04-16 11:54:36
- * @LastEditTime: 2022-09-23 14:35:04
+ * @LastEditTime: 2022-09-23 15:04:48
  */
 const apiUrl=process.env.REACT_APP_API_URL
 export const ProjectListScreen=()=>{
@@ -16,23 +16,26 @@ export const ProjectListScreen=()=>{
         name:"",
         personId:""
     })
+    const debouncedParam = useDebounce(param,2000)
     const [list,setList] = useState([])
 
     useEffect(()=>{
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response=>{
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response=>{
             if(response.ok){
                 setList(await response.json())
             }
         })
-    },[param])
-    // 初始化users
-    useEffect(()=>{
+    },[debouncedParam])
+
+    // 初始化users，组件加载时，运行一次【还是运行了两次】
+    
+    useMount(()=>{
         fetch(`${apiUrl}/users`).then(async response=>{
             if(response.ok){
                 setUsers(await response.json())
             }
         })
-    },[])
+    })
     
     return <div>
         <SearchPanel users={users} param={param} setParam={setParam}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,14 @@
 /*
  * @Description:
  * @Date: 2022-09-23 14:24:41
- * @LastEditTime: 2022-09-23 14:34:12
+ * @LastEditTime: 2022-09-23 15:19:30
  */
+
+import { useEffect, useState } from "react";
+
 // 判断值是不是0，不是则取反变成布尔值
 export const isFalsy = (value) => (value === 0 ? false : !value);
+
 // 不修改传入对象本身
 export const cleanObject = (object) => {
   const result = { ...object };
@@ -17,3 +21,48 @@ export const cleanObject = (object) => {
   });
   return result;
 };
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// 将value转换为debouncedValue
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  // 每次value变化时设置一个定时器
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行。类比第一个设置的timeout被第二个清理，第二个被第三个清理，只有最后一个可以保留下来
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+// const debounce = (func,delay)=>{
+//     let timeout;
+//     return ()=>{
+//         if(timeout){ // 一开始timeout是undefined，跳过
+//             clearTimeout(timeout);
+//         }
+//         timeout = setTimeout(function(){
+//             func();
+//         },delay)
+//     }
+// }
+
+// const log = debounce(()=>console.log('call'),5000)
+// log()
+// log()
+// log()
+
+// 原理讲解
+// 0s-------->1s-------->2s-------->3s-------->....
+// 三个函数同步操作，所以它们都是再0-1s时间段内瞬间完成
+// log()#1  // timeout#1
+// log()#2  // 发现 timeout#1！ 取消，设置timeout#2
+// log()#3  // 发现 timeout#2！ 取消，设置timeout#3
+//          // 所以，log()#3 结束后，只剩timeout#3在独自等待
